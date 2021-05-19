2c8997ab7b11fc31aa788a51a4b9c6c418c03f88
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-05-19 18:54:12 +0200

    Start page, nav, config

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from "react-native";
+import Constants from "expo-constants";
+
+import HomeScreen from "./screens/Home";
+import FormScreen from "./screens/Form";
+import ListScreen from "./screens/List";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Start" }}
+        />
+        <Stack.Screen
+          name="Form"
+          component={FormScreen}
+          options={{ title: "Form" }}
+        />
+        <Stack.Screen
+          name="List"
+          component={ListScreen}
+          options={{ title: "List" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
