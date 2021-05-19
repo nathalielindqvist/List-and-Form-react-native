d4947353af866299428ddb33e25a6f471bb2716a
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-05-19 18:54:55 +0200

    List

---
+++ screens/List.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  Button,
+} from "react-native";
+import { SearchBar, ListItem } from "react-native-elements";
+
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // If inserted text is not blank =>
+      // Filter the masterDataSource
+      // Update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        const itemData = item.name ? item.name.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // If inserted text is blank =>
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      <ListItem bottomDivider>
+        <Text style={styles.itemStyle} onPress={() => showItem(item)}>
+          {item.id}
+          {". "}
+          {item.name.toUpperCase()}
+        </Text>
+      </ListItem>
+    );
+  };
+
+  const showItem = (item) => {
+    alert(
+      "Id : " +
+        item.id +
+        "\n" +
+        "Name : " +
+        item.name +
+        "\n" +
+        "Email : " +
+        item.email +
+        "\n" +
+        "Phone : " +
+        item.phone
+    );
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <SearchBar
+          round
+          searchIcon={{ size: 24 }}
+          onChangeText={(text) => searchFilterFunction(text)}
+          onClear={(text) => searchFilterFunction("")}
+          placeholder="Search users"
+          value={search}
+        />
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    marginBottom: 100,
+  },
+  itemStyle: {
+    padding: 10,
+  },
+});
+
+export default App;
